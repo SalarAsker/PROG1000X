f3cec609a4008a16c219aeb20e54313e17117744
Salar Asker Zada <salar.askerzada@uddevalla.se>
2025-10-02 17:21:00 +0200

    create prog_14.cs byggstenar

---
+++ Övningar_Byggstenar/prog_14.cs
@@ -0,0 +1,12 @@
+Console.Write($"ange tal{++treTalRaknare}: ");
+int tal1 = int.Parse(Console.ReadLine());
+
+Console.Write($"ange tal{++treTalRaknare}: ");
+int tal2 = int.Parse(Console.ReadLine());
+
+Console.Write($"ange tal{++treTalRaknare}: ");
+int tal3 = int.Parse(Console.ReadLine());
+
+bool ordning = tal1 <= tal2 && tal2 <= tal3;
+
+Console.WriteLine($"Talen är i ordingsöljd: {ordning}");
